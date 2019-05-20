48b25ceec5537fe4b8d44f0fcfb132b15f6528ca
SSAAVVLL <SSAAVVLL@bk.ru>
2019-05-20 17:00:39 +0500

    Added copy of bootstrap styles

--- src/BootstrapAsset.php
+++ src/BootstrapAsset.php
@@ -17,8 +17,7 @@ use yii\web\AssetBundle;
  */
 class BootstrapAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/bootstrap/dist';
     public $css = [
-        'css/bootstrap.css',
+        'bootstrap.css',
     ];
 }
